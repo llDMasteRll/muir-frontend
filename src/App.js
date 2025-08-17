c5a5b1461f64717f8843f1ce13bbbf245b760e2d
llDMasteRll <mr.denys.popov@gmail.com>
2025-08-17 15:22:16 +0100

    main css styled now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './pages/Home.jsx'; // Adjust the import path as necessary
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
